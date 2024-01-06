7008630dd7316568565701dd25d2bc5e5166d443
Heaf <37232290+CAHeaf@users.noreply.github.com>
2024-01-05 23:33:21 -0700

    Update main.py
    Color palatte is now dynamically assigned, allowing for most drawing apps to work with this

--- main.py
+++ main.py
@@ -3,20 +3,12 @@ import pyautogui
 import keyboard
 import time
 
-paint_colors = [
-        (0, 0, 0), (127, 127, 127), (136, 0, 21), (237, 28, 36), (255, 127, 39),
-        (255, 242, 0), (34, 177, 76), (0, 162, 232), (63, 72, 204), (163, 73, 164),
-        (255, 255, 255), (195, 195, 195), (185, 122, 87), (255, 174, 201), (255, 201, 14),
-        (239, 228, 176), (181, 230, 29), (153, 217, 234), (112, 146, 190), (200, 191, 231)
-        ]
-color_positions = [[760, 60], [780, 60], [800, 60], [825, 60], [850, 60], [875, 60],
-                   [900, 60], [916, 60], [937, 60], [960, 60], [760, 83], [780, 83], 
-                   [800, 83], [825, 83], [850, 83], [875, 83], [900, 83],
-                   [916, 83], [937, 83], [960, 83]]
+paint_colors = []
+color_positions = []
 
 
 def get_image():
-    with Image.open('plant.png') as img:
+    with Image.open('furry.png') as img:
         img = img.convert('RGB')
         width, height = img.size
         pixels = list(img.getdata())
@@ -26,36 +18,19 @@ def get_image():
 
 def find_pallette():
 
-    print('Please hover over the middle of white on the color pallette and tap p')
+    print('Please hover over each color on the color pallette and tap p, press f when done')
+    i = 0
     while True:
-        if keyboard.is_pressed('p'):
-            white_x, white_y = pyautogui.position()
+        if keyboard.is_pressed('f'): 
             break
-    time.sleep(1)     
-    print('Please hover over the middle of black on the color pallette and tap p')
-    while True:
         if keyboard.is_pressed('p'):
-            black_x, black_y = pyautogui.position()
-            break
-    time.sleep(1)             
-    distance = abs(black_y - white_y)     
-
-          
-    create_pallette(black_x, black_y, white_y, white_x, distance)
+            x, y = pyautogui.position()
+            color_positions.append([x, y])
+            R, G, B = pyautogui.pixel(x, y)
+            paint_colors.append((R, G, B))
+            print(f'Color: {R, G, B} saved at {x, y}')
+            time.sleep(1)
     
-def create_pallette(black_x, black_y, white_y, white_x, distance):
-    for i in range(len(color_positions)):
-        print(distance)
-        if i <= 9:
-           color_positions[i][0] = black_x + distance * i + (distance * 0.1)
-           color_positions[i][1] = black_y
-        elif i > 9:
-           color_positions[i][0] = white_x + distance * i + (distance * 0.1)
-           color_positions[i][1] = white_y
-        color_positions[10][0] = white_x
-        color_positions[10][1] = white_y
-        print(color_positions[i])
-
 
 
 def change_colors(pixels):
@@ -74,7 +49,7 @@ def change_colors(pixels):
 def draw(pixels):
     #get mouse pos
     start_x, start_y = pyautogui.position()
-    pyautogui.PAUSE = 0.01
+    pyautogui.PAUSE = 0.001
     current_color = None
     
     # go through and draw each pixel, havent added color yet
@@ -102,7 +77,7 @@ def draw(pixels):
 
 def main():
     #not working atm
-    #find_pallette()
+    find_pallette()
     print('Place mouse where top left of image should be and then press P to begin')
     while True:
         if keyboard.is_pressed('p'):  
